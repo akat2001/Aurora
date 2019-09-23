03fae5cd3096d7e8797855e8f5a3c97cc9a36ab7
Gustavo Moreira Santos <gustavo.santos667@outlook.com>
2019-09-23 17:15:02 -0300

    Leve atualização na foto do user
    Ainda ñ pronto

--- DAL/Perfil/Class_inserirUfoto.php
+++ DAL/Perfil/Class_inserirUfoto.php
@@ -1,7 +1,12 @@
 <?php
     try
     {
-    InserirUfoto();
+        $UimgNNo = $_SESSION['User_Name'] . $UimgExt; //Nomeia o arquivo
+        $UimgPasta = "../../uploads/img_Uperf"; //Defini o nome da pasta em que o arquivo será salvo
+                    
+        move_uploaded_file($_FILES['Uimg']['tmp_name'], $UimgPasta.$UimgNNo);//efetua o upload
+
+        $_SESSION['UimgNNo'] = $UimgNNo; //Amazena o novo nome na sessão
     }
     catch(Exception $ex)
     {
